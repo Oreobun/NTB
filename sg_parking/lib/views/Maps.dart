cfc631dfd6b09bffef6746abdc9e0fd148656aab
dazz-lynn <37522735+dazz-lynn@users.noreply.github.com>
2020-03-14 23:52:44 +0800

    Google Maps

---
+++ sg_parking/lib/views/Maps.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Maps extends StatelessWidget {
+  Completer<GoogleMapController> _controller = Completer();
+  double zoomVal = 10.0;
+  final LatLng _center = const LatLng(1.3521, 103.8198);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Maps Sample App'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _buildGoogleMaps(context),_zoomminusfunction(), _zoomplusfunction()
+        ]
+      ),
+    );
+  }
+  Future<void> _minus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: _center, zoom: zoomVal)));
+  }
+  Future<void> _plus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: _center, zoom: zoomVal)));
+  }
+  Widget _buildGoogleMaps(BuildContext context){
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 10.0,
+        ),
+        markers: { NTU  }, //to add automated markers based on search query results
+      ),
+    );
+  }
+  Marker NTU = Marker(
+    markerId: MarkerId('ntu'),
+    position: LatLng(1.3483, 103.6831),
+    infoWindow: InfoWindow(title: 'Los Tacos'),
+    icon: BitmapDescriptor.defaultMarkerWithHue(
+      BitmapDescriptor.hueViolet,
+    ),
+  );
+
+  Widget _zoomminusfunction() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+          icon: Icon(Icons.zoom_out),
+          onPressed: () {
+            zoomVal--;
+            _minus( zoomVal);
+          }),
+    );
+  }
+  Widget _zoomplusfunction() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+          icon: Icon(Icons.zoom_in),
+          onPressed: () {
+            zoomVal++;
+            _plus(zoomVal);
+          }),
+    );
+  }
+
+  Marker NTU= Marker(
+    markerId: MarkerId('NTU'),
+    position: LatLng(1.3483,103.6831),
+    infoWindow: InfoWindow(title: 'NTU')
+  );
+}
+
--- sg_parking/lib/views/home.dart
+++ sg_parking/lib/views/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'TutorialPage.dart';
 import 'SearchDisplayPage(list).dart';
 import 'SearchDisplayPage(map).dart';
+import 'Maps.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -30,6 +31,9 @@ class _HomeState extends State<Home> {
       _currentPage = _listPages[selectedIndex];
     });
   }
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Maps()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +46,12 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.all(12.0),
           child: _currentPage,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.red[600],
+        child: Icon(Icons.location_on),
+        onPressed: () {navigateToSubPage(context);},
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
